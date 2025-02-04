4cf4f53ff379e77b0917dfa88a3a3c33f7fd51e2
Illtyd Wynn <illtyd@mail.wynnilltyd.xyz>
2025-02-04 14:02:58 -0800

    Added static functions to vec2def in sketch

--- learning_sketch.js
+++ learning_sketch.js
@@ -1,4 +1,5 @@
-ample is not part of the C++ project. As such it is not included
+/*
+  This sample is not part of the C++ project. As such it is not included
   in the src/ folder. To use it, copy its contents into editor.p5js.org,
   and then run.
   
@@ -9,6 +10,78 @@ ample is not part of the C++ project. As such it is not included
 */
 
 class vec2def {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  
+  static add(a, b) {
+    let result = new vec2def(0, 0);
+    
+    result.x = a.x + b.x;
+    result.y = a.y + b.y;
+    
+    return result;
+  }
+  
+  add(v) {
+    this.x += v.x;
+    this.y += v.y;
+  }
+  
+  static sub(a, b) {
+    let result = new vec2def(0, 0);
+    
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+    
+    return result;
+  }
+  
+  sub(v) {
+    this.x -= v.x;
+    this.y -= v.y;
+  }
+  
+  static scale(v, n) {
+    let result = new vec2def(0, 0);
+    
+    result.x = v.x * n;
+    result.y = v.y * n;
+    
+    return result;
+  }
+  
+  scale(n) {
+    this.x *= n;
+    this.y *= n;
+  }
+  
+  magnitude() {
+    return Math.sqrt((this.x * this.x) + (this.y * this.y));
+  }
+  
+  draw(color) {
+    fill(color);
+    stroke("white");
+    circle(this.x, this.y, 10);
+  }
+}
+
+let position = new vec2def(10, 20);
+let velocity = new vec2def(2, 1);
+
+function setup() {
+  createCanvas(400, 400);
+  
+  position = vec2def.scale(position, 4);
+}
+
+function draw() {
+  background("black");
+  
+  position.draw("red");
+}
   constructor(x, y) {
     this.x = x;
     this.y = y;
